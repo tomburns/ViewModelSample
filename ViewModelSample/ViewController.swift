0d85c5c678232c1bc1a84579c52592c5fdd3f329
Tom Burns <tomburns@gmail.com>
2018-08-20 18:17:01 -0500

    Initial Commit

---
+++ ViewModelSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewModelSample
+//
+//  Created by Tom Burns on 8/20/18.
+//  Copyright © 2018 ActiveCampaign. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
